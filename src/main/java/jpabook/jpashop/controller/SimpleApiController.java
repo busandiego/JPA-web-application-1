5d36637836ba90bf7efa8367ba878c9d17d610ef
이수인 <silee@weedscomm.com>
2021-04-10 18:17:23 +0900

    간단한 주문 조회 V2: 엔티티를 DTO로 변환, N + 1 문제 발생

--- src/main/java/jpabook/jpashop/controller/SimpleApiController.java
+++ src/main/java/jpabook/jpashop/controller/SimpleApiController.java
@@ -1,25 +1,30 @@
 package jpabook.jpashop.controller;
 
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne ( ManyToOne, OneToOne)
- * Order 
+ * Order
  * Order -> Member
  * Order -> Delivery
  */
 @RestController
 @RequiredArgsConstructor
-public class SimpleApiController{
-    
+public class SimpleApiController {
+
     private final OrderRepository orderRepository;
 
     @GetMapping("/api/vi/simple-orders")
@@ -30,10 +35,48 @@ public class SimpleApiController{
             order.getMember().getName(); // Lazy 강제 초기화
             order.getDelivery().getAddress(); // Lazy 강제 초기화
         }
-        
-        
-        
         return all;
-        
+
     }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+
+        // order 2개 조회
+
+        // N + 1 문제 첫번째 쿼리가 나감 N번만큼 가져옴
+        // N + 1  + 회원 N + 배송 N => 5
+        // Eager로 하면 예측이 안됨.
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        //
+        List<SimpleOrderDto> result = orders.stream()
+                    .map(o -> new SimpleOrderDto(o))
+                    .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        // DTO가 엔티티를 파라미터로 받는 것은 크게 문제가 되지 않는다.
+        // 생성자에다가 DTO의 멤버변수를 다 set함
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); // LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); // LAZY 초기화
+
+        }
+    }
+
+
 }
